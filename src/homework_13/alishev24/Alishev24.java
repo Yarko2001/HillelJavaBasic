a7cf3f8880b1f771bf5be74c2625105a80b3829c
Degtiar Yaroslav <merrynan2001@gmail.com>
2022-07-10 20:51:00 +0300

    fix mistakes

---
+++ src/homework_13/alishev24/Alishev24.java
@@ -0,0 +1,15 @@
+package homework_13.alishev24;
+
+public class Alishev24 {
+    private String name;
+    private int age;
+
+    public Alishev24(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String toString() {
+        return name + "," + age;
+    }
+}
---
+++ src/homework_13/alishev24/Main.java
@@ -0,0 +1,10 @@
+package homework_13.alishev24;
+
+public class Main {
+    public static void main(String[] args) {
+        Alishev24 human1 = new Alishev24("Yaroslav",21);
+        System.out.println(human1);
+        Alishev24 human2 = new Alishev24("Elena",25);
+        System.out.println(human2);
+    }
+}
